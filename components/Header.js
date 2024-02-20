a6c5687648c0f8b2e27e917215b60250d155632e
JonBrown95 <126886504+JonBrown95@users.noreply.github.com>
2024-02-20 20:31:25 +0000

    Updated Header and restyled

---
+++ components/Header.js
@@ -0,0 +1,15 @@
+const Header = () => {
+  return (
+    <header className="fixed top-0 left-0 right-0 z-10 py-4 bg-white">
+      <div className="w-full px-10">
+        <div className="flex items-center justify-between">
+          <div className="font-bold text-sm text-black uppercase tracking-wider">
+            Earth Angel
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
