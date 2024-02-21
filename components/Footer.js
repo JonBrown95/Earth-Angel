52c934a901d419049e3bf18f300623e238097ef7
JonBrown95 <126886504+JonBrown95@users.noreply.github.com>
2024-02-21 22:11:29 +0000

    Created basic footer and added to _app.js

---
+++ components/Footer.js
@@ -0,0 +1,16 @@
+const Footer = () => {
+  return (
+    <footer className="py-8 bg-gray-900 text-white">
+      <div className="container mx-auto px-4">
+        <div className="flex flex-col lg:flex-row justify-between items-center"></div>
+        <hr className="border-gray-800 mt-6 mb-4" />
+        <div className="text-center text-sm tracking-wider">
+          <p>Earth Angel - made with love </p>
+          <p className="mt-2">Designed by Jonny Brown</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
